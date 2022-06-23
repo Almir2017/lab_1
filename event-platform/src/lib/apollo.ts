5552bab5b6d7493e468188208449a2b9c277bef6
Almir2017 <allmir_silva@hotmail.com>
2022-06-23 07:53:16 -0300

    ulr

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+    uri: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     cache: new InMemoryCache()
 })
