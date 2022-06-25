c3738729b77f8f40dfbc964b9cef95d4e2504967
Almir <allmir_silva@hotmail.com>
2022-06-25 10:38:48 -0300

    Update apollo.ts

--- event-platform/src/lib/apollo.ts
+++ event-platform/src/lib/apollo.ts
@@ -1,13 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-<<<<<<< HEAD
     uri: import.meta.env.VITE_API_URL,
     headers: {
         'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
     },
-=======
-    uri: 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
->>>>>>> a33c02b621540d17f93c3186a64bb399429f8fa5
     cache: new InMemoryCache()
 })
